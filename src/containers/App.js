fe4086406bd2f7dc69b6a2d89c6551be6b1b519b
robertschaap <robert.schaap@mac.com>
2018-06-26 20:33:11 +0200

    Replaced Aux component with Fragment

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Aux from "../containers/Aux";
 
 import ReadPage from "./ReadPage";
 import WritePage from "./WritePage";
@@ -9,11 +8,11 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Aux>
+        <Fragment>
           <Route exact path="/" component={ReadPage} />
           <Route path="/writesomething" component={WritePage} />
           <Route path="/readsomething" component={ReadPage} />
-        </Aux>
+        </Fragment>
       </BrowserRouter>
     );
   }
--- src/containers/Aux.js
+++
@@ -1,3 +0,0 @@
-// This serves only to reduce Div-ception
-const Aux = ({ children }) => children;
-export default Aux;
