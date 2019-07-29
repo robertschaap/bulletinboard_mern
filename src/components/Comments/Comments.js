c9a1474eea5a56d6c8b297756ecafaa9a7bc8634
robertschaap <robert.schaap@mac.com>
2019-07-29 21:23:10 +0200

    Comments: refactored to SFC

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Comments.scss";
 
 import Comment from "../../components/Comment/";
 
-class Comments extends Component {
-  render() {
-    const {
+const Comments = (props) => {
+  const {
       comments,
       onLoadComments,
       onSortComments,
       sortDirection
-    } = this.props;
+    } = props;
 
-    return (
-      <main className={styles.component}>
-        <section className={styles.wrapper}>
-          <h2>Read Something</h2>
-          <select
-            className={styles.sortbtn}
-            value={sortDirection}
-            onChange={onSortComments}>
-              <option value="desc">Newest First</option>
-              <option value="asc">Oldest First</option>
-          </select>
-          {comments.map((item, index) => (
-            <Comment
-              key={index}
-              image={item.avatar}
-              title={item.title}
-              name={item.name}
-              body={item.body} />
-          ))}
-        </section>
+  return (
+    <main className={styles.component}>
+      <section className={styles.wrapper}>
+        <h2>Read Something</h2>
+        <select
+          className={styles.sortbtn}
+          value={sortDirection}
+          onChange={onSortComments}>
+            <option value="desc">Newest First</option>
+            <option value="asc">Oldest First</option>
+        </select>
+        {comments.map((item, index) => (
+          <Comment
+            key={index}
+            image={item.avatar}
+            title={item.title}
+            name={item.name}
+            body={item.body} />
+        ))}
+      </section>
 
-        <section className={styles.wrapper}>
-          <button onClick={onLoadComments}>
-            Load More Comments
-          </button>
-        </section>
-      </main>
-    );
-  }
-}
+      <section className={styles.wrapper}>
+        <button onClick={onLoadComments}>
+          Load More Comments
+        </button>
+      </section>
+    </main>
+  );
+};
 
 export default Comments;
