1e50572bc4e22a950dc75f7e3dfffdd59b0d11b5
robertschaap <robert.schaap@mac.com>
2019-07-29 20:50:27 +0200

    ReadPageContainer: refactored to class component

--- src/containers/ReadPageContainer.js
+++ src/containers/ReadPageContainer.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Comments from "../components/Comments";
 
-const ReadPageContainer = () => (
-    <Comments />
-);
+class ReadPageContainer extends React.Component {
+  render() {
+    return (
+        <Comments />
+    );
+  }
+}
 
 export default ReadPageContainer;
