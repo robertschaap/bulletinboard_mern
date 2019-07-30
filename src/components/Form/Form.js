c76edd9582e03f58f6223e09cd3a025c59530030
robertschaap <robert.schaap@mac.com>
2019-07-30 19:15:43 +0200

    Form: refactored to SFC

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,59 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Form.scss";
 
-class Form extends Component {
-  render() {
-    const {
-      avatar,
-      onChange,
-      onSubmit,
-    } = this.props;
+const Form = (props) => {
+  const {
+    avatar,
+    onChange,
+    onSubmit,
+  } = props;
 
-    return (
-      <main className={styles.component}>
-        <section>
-          <h2>Write Something</h2>
-          <form onSubmit={onSubmit} className={styles.form}>
+  return (
+    <main className={styles.component}>
+      <section>
+        <h2>Write Something</h2>
+        <form onSubmit={onSubmit} className={styles.form}>
 
-            <label>What's your name?:</label>
-            <input
-              type="text"
-              name="name"
-              onChange={onChange} />
+          <label>What's your name?:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={onChange} />
 
-            <label>Write a title (max 100 char):</label>
-            <input
-              type="text"
-              name="title"
-              onChange={onChange} />
+          <label>Write a title (max 100 char):</label>
+          <input
+            type="text"
+            name="title"
+            onChange={onChange} />
 
-            <label>Share your message:</label>
-            <textarea
-              type="text"
-              name="body"
-              onChange={onChange} />
+          <label>Share your message:</label>
+          <textarea
+            type="text"
+            name="body"
+            onChange={onChange} />
 
-            <label>Select an avatar:</label>
-            <select
-              name="avatar"
-              value={avatar}
-              onChange={onChange}>
-              <option value="bunny">A cute bunny</option>
-              <option value="elephant">A fierce elephant</option>
-              <option value="hippo">A loud hippo</option>
-              <option value="hyena">A laughing hyena</option>
-              <option value="kitty">A rofl-ing kitty</option>
-              <option value="puppy">A bashful puppy</option>
-              <option value="sheep">The master race</option>
-            </select>
+          <label>Select an avatar:</label>
+          <select
+            name="avatar"
+            value={avatar}
+            onChange={onChange}>
+            <option value="bunny">A cute bunny</option>
+            <option value="elephant">A fierce elephant</option>
+            <option value="hippo">A loud hippo</option>
+            <option value="hyena">A laughing hyena</option>
+            <option value="kitty">A rofl-ing kitty</option>
+            <option value="puppy">A bashful puppy</option>
+            <option value="sheep">The master race</option>
+          </select>
 
-            <button type="submit">Submit</button>
-          </form>
-        </section>
-      </main>
-    );
-
-  }
-}
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
 export default Form;
