ee8be46f84895535b17ef30059810d7801b3e4fb
robertschaap <robert.schaap@mac.com>
2019-07-30 19:14:03 +0200

    WritePageContainer: extract business logic from Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,80 +1,43 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import styles from "./Form.scss";
 
 class Form extends Component {
-  state = {
-    formData: {
-      name: "",
-      title: "",
-      body: "",
-      avatar: "bunny",
-    },
-    doRedirect: false
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    fetch("/api/comment/new", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(this.state.formData)
-    })
-    .then(() =>
-      this.setState({ doRedirect: true })
-    );
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      formData: {
-        ...this.state.formData,
-        [event.target.name]: event.target.value
-      }
-    });
-  }
-
   render() {
-
-    const doRedirect = this.state.doRedirect;
-    if (doRedirect) {
-      return (
-        <Redirect to="/readsomething" push />
-      );
-    }
+    const {
+      avatar,
+      onChange,
+      onSubmit,
+    } = this.props;
 
     return (
       <main className={styles.component}>
         <section>
           <h2>Write Something</h2>
-          <form onSubmit={this.handleSubmit} className={styles.form}>
+          <form onSubmit={onSubmit} className={styles.form}>
 
             <label>What's your name?:</label>
             <input
               type="text"
               name="name"
-              onChange={this.handleChange} />
-        
+              onChange={onChange} />
+
             <label>Write a title (max 100 char):</label>
             <input
               type="text"
               name="title"
-              onChange={this.handleChange} />
-        
+              onChange={onChange} />
+
             <label>Share your message:</label>
             <textarea
               type="text"
               name="body"
-              onChange={this.handleChange} />
-        
+              onChange={onChange} />
+
             <label>Select an avatar:</label>
             <select
               name="avatar"
-              value={this.state.avatar}
-              onChange={this.handleChange}>
+              value={avatar}
+              onChange={onChange}>
               <option value="bunny">A cute bunny</option>
               <option value="elephant">A fierce elephant</option>
               <option value="hippo">A loud hippo</option>
@@ -83,7 +46,7 @@ class Form extends Component {
               <option value="puppy">A bashful puppy</option>
               <option value="sheep">The master race</option>
             </select>
-            
+
             <button type="submit">Submit</button>
           </form>
         </section>
--- src/containers/WritePageContainer.js
+++ src/containers/WritePageContainer.js
@@ -1,10 +1,57 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+
 import Form from "../components/Form";
 
 class WritePageContainer extends React.Component {
+  state = {
+    formData: {
+      name: "",
+      title: "",
+      body: "",
+      avatar: "bunny",
+    },
+    doRedirect: false
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("/api/comment/new", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state.formData)
+    })
+    .then(() =>
+      this.setState({ doRedirect: true })
+    );
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
+
   render() {
+    const { avatar, doRedirect } = this.state;
+
+    if (doRedirect) {
+      return (
+        <Redirect to="/readsomething" push />
+      );
+    }
+
     return (
-      <Form />
+      <Form
+        avatar={avatar}
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit} />
     );
   }
 }
