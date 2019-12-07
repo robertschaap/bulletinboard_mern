f18fe4dd72c982042a29b3a68c4b67eaf96f82a5
robertschaap <robert.schaap@mac.com>
2019-12-07 21:51:44 +0100

    index: linting fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
 
-import { Provider } from "react-redux";
-import { store } from "./ducks/redux";
+import { Provider } from 'react-redux';
+import { store } from './ducks/redux';
 
-import "./styles/normalize.css";
-import "./styles/index.scss";
+import './styles/normalize.css';
+import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
